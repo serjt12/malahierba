2adaddfd0dfcaaba4f1182d5a36963e21165fc1f
serjt12 <jandreus87@hotmail.com>
2019-03-06 06:59:38 -0500

    adding side bar

--- index.js
+++ index.js
@@ -38,4 +38,43 @@ $(document).ready(function() {
           $('.drawer').drawer('toggle')
       });
   })
+  $('.random').on('click', function() {
+    var page = $(this).data('page');
+    if(page === undefined) {
+      var a = 1+Math.round(Math.random()*10);
+      if (a==1) page = "page1"
+      if (a==2) page ="page2"
+  	  if (a==3) page ="page3"
+  	  if (a==4) page ="page4"
+  	  if (a==5) page ="page5"
+  	  if (a==6) page ="page6"
+  	  if (a==7) page ="page7"
+  	  if (a==8) page ="page8"
+  	  if (a==9) page ="page9"
+  	  if (a==10) page ="page10"
+  	  if (a==11) page ="page11"
+      $(".main-content .page:not('.hide-content')").stop().fadeOut('fast', function() {
+          $(this).addClass('hide-content');
+          $('.main-content .page[data-page="'+page+'"]').fadeIn('slow').removeClass('hide-content');
+      });
+      $.router.set({
+        route: '#/playlist',
+        queryString: 'q=' + page
+      });
+    }
+  })
+  $('.enlaces-toggle').on('click', function() {
+    $('.enlaces').toggleClass('hide-enlaces')
+    $('.info').addClass('hide-info')
+  })
+  $('.info-toggle').on('click', function() {
+    $('.enlaces').addClass('hide-enlaces')
+    $('.info').toggleClass('hide-info')
+  })
+  $('.close-enlaces').on('click', function() {
+    $('.enlaces').toggleClass('hide-enlaces')
+  })
+  $('.close-info').on('click', function() {
+    $('.info').toggleClass('hide-info')
+  })
 });
